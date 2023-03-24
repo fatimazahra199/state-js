1eeaa5ddd799d0e568ed5a0a7044bba3873d97b7
Fatima Zahra <fatima.zahra.faryat199@gmail.com>
2023-03-24 21:28:21 +0100

    using states in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: "Fatima Zahra Faryat",
+        bio: "Hi, I'm a FullStack Developer & UI/UX designer",
+        imgSrc:
+          "https://i.pinimg.com/originals/ca/63/1c/ca631c51a4dfa41c7964eb38bc831f3a.jpg",
+        profession: "Web Developer",
+      },
+      Shows: false,
+      mountTime: new Date(),
+      // currentTime: new Date(),
+      interval: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { mountTime } = this.state;
+      const currentTime = new Date();
+      const interval = Math.floor(
+        (currentTime.getTime() - mountTime.getTime()) / 1000
+      );
+      this.setState({ interval });
+    }, 1000);
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({ Shows: !prevState.Shows }));
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+  }
+
+  render() {
+    return (
+      <div className='profile'>
+        <button className="toggle" onClick={this.handleClick}>Toggle Profile</button>
+        {this.state.Shows && (
+          <div className='profile_items'>
+            <img className="image" src={this.state.Person.imgSrc} alt="profile" />
+            <h1>{this.state.Person.fullName}</h1>
+            <h2>{this.state.Person.profession}</h2>
+            <p>{this.state.Person.bio}</p>
+          </div>
+        )}
+        <p>Component mounted {this.state.interval} seconds ago.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
